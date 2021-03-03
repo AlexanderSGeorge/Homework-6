102db3e873fbf00e78fed062f13d83ac10260a68
AlexanderSGeorge <Alex.George@student.csulb.edu>
2021-03-02 17:48:58 -0800

    intial release for homework 6

---
+++ js/hw6.1.js
@@ -0,0 +1,6 @@
+//loop through child nodes
+const bodyNodes = document.body.childNodes;
+
+for (let i = 0; i < bodyNodes.length; i++) {
+    console.log(bodyNodes[i]);
+  };
---
+++ js/hw6.2.js
@@ -0,0 +1,9 @@
+//Complete code for the following function
+//Display text for 400 level courses
+
+const displayText400 = Array.from(document.getElementsByClassName("400level"));
+displayText400.forEach(element => {
+  console.log(element.textContent);
+});
+
+
